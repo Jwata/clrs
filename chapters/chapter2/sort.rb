912781fddd43bd59bc03d27feb474bb442b6dc44
Jwata <watanabe0621@gmail.com>
2017-02-10 00:51:52 +0900

    Exercises 2.1-2 insertion sort reverse

--- chapters/chapter2/sort.rb
+++ chapters/chapter2/sort.rb
@@ -5,6 +5,10 @@ module Sort
     list.inject([]) { |sorted_list, a| insert(a, sorted_list) }
   end
 
+  def self.insertion_reverse(list)
+    list.inject([]) { |sorted_list, a| insert_reverse(a, sorted_list) }
+  end
+
   private
 
   def self.insert(a, list)
@@ -15,6 +19,17 @@ module Sort
       [a, b].concat list
     else
       [b].concat insert(a, list)
+    end
+  end
+
+  def self.insert_reverse(a, list)
+    return [] if a.nil?
+    return [a] if list.empty?
+    b = list.shift
+    if a > b
+      [a, b].concat list
+    else
+      [b].concat insert_reverse(a, list)
     end
   end
 end
